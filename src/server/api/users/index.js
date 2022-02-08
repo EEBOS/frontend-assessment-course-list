0dd42ca5747bd353694378b982def85173e7f6a3
aubeOSG <caube@osg.ca>
2022-02-08 17:20:03 -0500

    [sc-178] UPDATE - added user course endpoint and basic user course data

--- src/server/api/users/index.js
+++ src/server/api/users/index.js
@@ -7,13 +7,24 @@ const files = {
     basePath: 'src/server/api/users'
 }
 
+routes.courses = routes.base + '/courses'
 files.user = files.basePath + '/data/user.json'
+files.courses = files.basePath + '/data/courses.json'
 
 export const init = (router) => {
     router.get(routes.base, (req, res) => {
         
         try {
             res.json(fs.getFile(files.user))
+        } catch (err) {
+            res.send(err)
+        }
+    })
+
+    router.get(routes.courses, (req, res) => {
+
+        try {
+            res.json(fs.getFile(files.courses))
         } catch (err) {
             res.send(err)
         }
