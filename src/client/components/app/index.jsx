72f7be7a91958ef088d0be61193f2d76fefeb304
aubeOSG <caube@osg.ca>
2022-03-04 09:55:49 -0500

    UPDATE [sc-278] - implemented a basic react app

---
+++ src/client/components/app/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import pageCourses from '../../pages/courses'
+
+export const App = () => {
+  return (
+    <Router basename='/'>
+      <main className="app-content">
+        <Routes>
+          <Route path={`${pageCourses.Route}`} element={<pageCourses.Page />}/>
+        </Routes>
+      </main>
+    </Router>
+  )
+}
+
+export default {
+  App
+}
---
+++ src/client/pages/courses/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Route = '/'
+
+export const Page = () => {
+  return (
+    <>
+      <div>List of Courses</div>
+    </>
+  )
+}
+
+export default {
+  Route,
+  Page
+}
